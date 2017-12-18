a7cb3a64bb9edd698ef6cb13eadcaf7a34823be0
Hyun-Kyung Yi <hyun.yi@nb-hyi.xing.hh>
2017-12-18 17:57:26 +0100

    started hashing setup

--- server/server.js
+++ server/server.js
@@ -94,6 +94,23 @@ app.patch('/todos/:id', (req, res) => {
   });
 });
 
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+
+  // var user = new User({
+  //   email: body.email,
+  //   password: body.pasword
+  // });
+ //besser:
+  var user = new User(body);
+
+  user.save().then((doc) => {
+    res.send(doc);
+  }).catch((e) => {
+    res.status(400).send(e);
+  });
+});
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
